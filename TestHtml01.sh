f0433d094f13254f51badfdef27f88905754a99c
rendszermernok02 <71371084+rendszermernok02@users.noreply.github.com>
2020-09-17 19:19:38 +0200

    Add files via upload

---
+++ TestHtml01.sh
@@ -0,0 +1,7 @@
+RESOURCE_GROUP='rg5'
+
+AKS_CLUSTER='TestHtml01'
+
+az aks get-credentials --resource-group $RESOURCE_GROUP --name $AKS_CLUSTER
+
+kubectl expose deployment testhtml-deployment --port=80 --type=LoadBalancer
