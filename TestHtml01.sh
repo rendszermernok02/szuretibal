cb35f10ce490544d37d750dd620bdabd202de083
rendszermernok02 <71371084+rendszermernok02@users.noreply.github.com>
2020-09-17 19:41:00 +0200

    Update TestHtml01.sh

--- TestHtml01.sh
+++ TestHtml01.sh
@@ -1,7 +1,4 @@
 RESOURCE_GROUP='rg5'
-
 AKS_CLUSTER='TestHtml01'
-
 az aks get-credentials --resource-group $RESOURCE_GROUP --name $AKS_CLUSTER
-
 kubectl expose deployment testhtml-deployment --port=80 --type=LoadBalancer
