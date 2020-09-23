c04d9685b70b3479595a1d3232db36ad9585821e
rendszermernok02 <71371084+rendszermernok02@users.noreply.github.com>
2020-09-23 18:33:36 +0200

    Create LoadBalancer.sh

---
+++ LoadBalancer.sh
@@ -0,0 +1,19 @@
+# https://opensource.com/article/20/7/homelab-metallb
+
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/namespace.yaml
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/metallb.yaml
+kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
+
+cat <<EOF | kubectl create -f -
+> apiVersion: v1
+> kind: ConfigMap
+> metadata:
+>   name: config
+> data:
+>   config: |
+>     address-pools:
+>     - name: address-pool-1
+>       protocol: layer2
+>       addresses:
+>       - 192.168.0.192/28
+> EOF
