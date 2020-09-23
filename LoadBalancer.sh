ace9b0a2f2f36d6a7beda1be1cb675c850de5490
rendszermernok02 <71371084+rendszermernok02@users.noreply.github.com>
2020-09-23 19:15:26 +0200

    Update LoadBalancer.sh

--- LoadBalancer.sh
+++ LoadBalancer.sh
@@ -6,15 +6,15 @@ kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manife
 kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
 
 cat <<EOF | kubectl create -f -
-> apiVersion: v1
-> kind: ConfigMap
-> metadata:
->   name: config
-> data:
->   config: |
->     address-pools:
->     - name: address-pool-1
->       protocol: layer2
->       addresses:
->       - 192.168.0.192/28
-> EOF
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: config
+data:
+  config: |
+    address-pools:
+    - name: address-pool-1
+      protocol: layer2
+      addresses:
+      - 192.168.0.192/28
+EOF
