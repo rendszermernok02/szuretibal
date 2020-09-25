14f1d3095db9fbfb83e3ae8fac6815222adb4229
rendszermernok02 <71371084+rendszermernok02@users.noreply.github.com>
2020-09-25 15:34:58 +0200

    Update deployment.sh

--- deployment.sh
+++ deployment.sh
@@ -1,8 +1,3 @@
-RESOURCE_GROUP='rg5'
-AKS_CLUSTER='kube01'
-az aks get-credentials --resource-group $RESOURCE_GROUP --name $AKS_CLUSTER
-# kubectl expose deployment testhtml-deployment --port=80 --type=LoadBalancer
-
 RESOURCE_GROUP=rg5
 AKS_CLUSTER=kube01
 az aks get-credentials --resource-group $RESOURCE_GROUP --name $AKS_CLUSTER
