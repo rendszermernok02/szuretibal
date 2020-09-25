3b539b1b4c262867f6385639b6e0ddcae6a97df0
rendszermernok02 <71371084+rendszermernok02@users.noreply.github.com>
2020-09-25 15:51:32 +0200

    Update deployment.sh

--- deployment.sh
+++ deployment.sh
@@ -5,7 +5,7 @@ az aks get-credentials --resource-group $RESOURCE_GROUP --name $AKS_CLUSTER
 # Enter your details below.
 PIP_RESOURCE_GROUP=rg5
 AKS_RESOURCE_GROUP=rg5
-AKS_CLUSTER_NAME='kube01'
+AKS_CLUSTER_NAME=kube01
 # Do not change anything below this line
 CLIENT_ID=$(az aks show --resource-group $AKS_RESOURCE_GROUP --name $AKS_CLUSTER_NAME --query "servicePrincipalProfile.clientId" --output tsv)
 SUB_ID=$(az account show --query "id" --output tsv)
