583d4e568284d6da6c4f76a0fb8973e270259c1b
rendszermernok02 <71371084+rendszermernok02@users.noreply.github.com>
2020-09-25 11:09:12 +0200

    Update deployment.sh

--- deployment.sh
+++ deployment.sh
@@ -1,16 +1,16 @@
 RESOURCE_GROUP='rg5'
-AKS_CLUSTER='KubeService01'
+AKS_CLUSTER='kube01'
 az aks get-credentials --resource-group $RESOURCE_GROUP --name $AKS_CLUSTER
 # kubectl expose deployment testhtml-deployment --port=80 --type=LoadBalancer
 
 RESOURCE_GROUP=rg5
-AKS_CLUSTER=KubeService01
+AKS_CLUSTER=kube01
 az aks get-credentials --resource-group $RESOURCE_GROUP --name $AKS_CLUSTER
 
 # Enter your details below.
 PIP_RESOURCE_GROUP=rg5
 AKS_RESOURCE_GROUP=rg5
-AKS_CLUSTER_NAME='KubeService01'
+AKS_CLUSTER_NAME='kube01'
 # Do not change anything below this line
 CLIENT_ID=$(az aks show --resource-group $AKS_RESOURCE_GROUP --name $AKS_CLUSTER_NAME --query "servicePrincipalProfile.clientId" --output tsv)
 SUB_ID=$(az account show --query "id" --output tsv)
